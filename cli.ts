5d83541f20f2dcaadff7d0773a62b959c64d6759
Jonathan Delgado <hi@jon.soy>
2022-07-04 00:05:45 -0400

    feat: add command init

--- cli.ts
+++ cli.ts
@@ -6,20 +6,22 @@ import { resolveConfigsPaths } from "./utils/resolveConfigsPaths.ts";
 
 
 export async function bin(args: string[]) {
-    const ipmFile = await IPMFile.eachIPMFileFactory(resolveConfigsPaths())
     const argsOptions = parseArgs(args);
 
     if (argsOptions.args[0] === "info") {
+        const ipmFile = await IPMFile.eachIPMFileFactory(resolveConfigsPaths())
         console.log(ipmFile)
         return
     }
 
     if (argsOptions.args[0] === "pull") {
+        const ipmFile = await IPMFile.eachIPMFileFactory(resolveConfigsPaths())
         await new PullCollectionResource(ipmFile).pullResources(ipmFile.icons, ipmFile.outDir, ipmFile.formatOut);
         return
     }
-    
+
     if (argsOptions.args[0] === "init") {
+        await Deno.writeTextFile(`${Deno.cwd()}/ipm.yaml`, await Deno.readTextFile(new URL("./samples/ipm.yaml", import.meta.url)))
         return;
     }
 }
