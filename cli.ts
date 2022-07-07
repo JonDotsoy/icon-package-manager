66bd08ef63c0c9ff312a1f808cc56a8b9d19648d
Jonathan Delgado <hi@jon.soy>
2022-07-06 20:05:37 -0400

    feat: add dialog help to cli.ts

--- cli.ts
+++ cli.ts
@@ -1,31 +1,65 @@
-import { YAML } from "./deps.ts";
 import { PullCollectionResource } from "./target/icon-package-manager.ts"
 import { IPMFile } from "./utils/ipm-file.ts";
 import { parseArgs } from "./utils/parseArgs.ts";
 import { resolveConfigsPaths } from "./utils/resolveConfigsPaths.ts";
 
 
-export async function bin(args: string[]) {
-    const argsOptions = parseArgs(args);
+const helpMessage = `
+ipm <command>
 
-    if (argsOptions.args[0] === "info") {
+Usage:
+
+ipm init           Create a initial ipm.yaml file
+ipm pull           Download all assets and generate the icons files
+
+imp ${import.meta.url}`
+
+const seeListCommands = `
+To see a list of supported ipm commands, run:
+  ipm help`
+
+class CliError extends Error { }
+
+class UnknownCommand extends CliError {
+    constructor(readonly command: string) { super(`Unknown command: ${Deno.inspect(command)}`) }
+}
+
+export async function bin(args: string[]) {
+    const { args: [command], help } = parseArgs(args);
+
+    if (command === "info") {
         const ipmFile = await IPMFile.eachIPMFileFactory(resolveConfigsPaths())
         console.log(ipmFile)
         return
     }
 
-    if (argsOptions.args[0] === "pull") {
+    if (command === "pull") {
         const ipmFile = await IPMFile.eachIPMFileFactory(resolveConfigsPaths())
         await new PullCollectionResource(ipmFile).pullResources(ipmFile.icons, ipmFile.outDir, ipmFile.formatOut);
         return
     }
 
-    if (argsOptions.args[0] === "init") {
+    if (command === "init") {
         await Deno.writeTextFile(`${Deno.cwd()}/ipm.yaml`, await Deno.readTextFile(new URL("./samples/ipm.yaml", import.meta.url)))
         return;
     }
+
+    if (help) {
+        return console.log(helpMessage)
+    }
+
+    throw new UnknownCommand(command);
 }
 
 if (import.meta.main) {
-    await bin([...Deno.args])
+    try {
+        await bin([...Deno.args])
+    } catch (ex) {
+        if (ex instanceof UnknownCommand) {
+            console.log(`${ex.message}`)
+            console.log(seeListCommands)
+        } else {
+            throw ex;
+        }
+    }
 }
